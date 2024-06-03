75a505d49f16e483af26a7a2c47d5e1f296ad0e9
Urian de Castro Almeida <urian.castro@gmail.com>
2024-06-03 00:09:32 -0300

    Ajustes na conversão para Number

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ let nivel = "";
 const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
 const nomeHeroi = entrada("Digite o nome do seu Herói: ");
-let xpHeroi = entrada("Digite o valor do XP de seu herói. O valor deve iniciar em 1000: ");
+let xpHeroi = +entrada("Digite o valor do XP de seu herói. O valor deve iniciar em 1000: ");
 
-while(Number(xpHeroi) < 1000) {
-    xpHeroi = entrada("O valor do XP deve iniciar em 1000. Digite o valor novamente: ");
+while(xpHeroi < 1000) {
+    xpHeroi = +entrada("O valor do XP deve iniciar em 1000. Digite o valor novamente: ");
 }
 
 switch(true) {
-    case Number(xpHeroi) >= 10001:
+    case xpHeroi >= 10001:
         nivel = niveis[7];
         break;
-    case Number(xpHeroi) >= 9001 && Number(xpHeroi) <= 10000:
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
         nivel = niveis[6];
         break;
-    case Number(xpHeroi) >= 8001 && Number(xpHeroi) <= 9000:
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
         nivel = niveis[5];
         break;
-    case Number(xpHeroi) >= 7001 && Number(xpHeroi) <= 8000:
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
         nivel = niveis[4];
         break;
-    case Number(xpHeroi) >= 5001 && Number(xpHeroi) <= 7000:
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
         nivel = niveis[3];
         break;
-    case Number(xpHeroi) >= 2001 && Number(xpHeroi) <= 5000:
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
         nivel = niveis[2];
         break;
-    case Number(xpHeroi) >= 1001 && Number(xpHeroi) <= 2000:
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
         nivel = niveis[1];
         break;
     default:
         nivel = niveis[0];
 }
 
-console.log(`O herói de nome ${nomeHeroi} está no nível de ${nivel}.`);
+console.log(`O herói de nome ${nomeHeroi} está no nível ${nivel}.`);
